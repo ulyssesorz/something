b29c7f21b8bcff451020182333bb96617af4f843
zhanghao.557 <zhanghao.557@bytedance.com>
2024-06-20 10:43:13 +0800

    up

---
+++ multiprint.cpp
@@ -0,0 +1,39 @@
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#include <iostream>
+using namespace std;
+
+int flag = 0;
+std::mutex mtx;
+std::condition_variable cond;
+
+int main()
+{
+    thread t1([&]()
+    {
+        unique_lock<mutex> locker(mtx);
+        for(int i = 0; i < 20; i += 2)
+        {
+            cond.wait(locker, [&](){ return flag == 0; });
+            printf("%d ", i);
+            flag = 1;
+            cond.notify_one();
+        }
+    });
+    thread t2([&]()
+    {
+        unique_lock<mutex> locker(mtx);
+        for(int i = 1; i < 20; i += 2)
+        {
+            cond.wait(locker, [&](){ return flag == 1; });
+            printf("%d ", i);
+            flag = 0;
+            cond.notify_one();
+        }
+    });
+    t1.detach();
+    t2.detach();
+    this_thread::sleep_for(chrono::seconds(2));
+}
