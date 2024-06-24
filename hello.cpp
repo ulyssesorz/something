00a3209fb9c50c54e0db787b3cfd5b72d52f61c4
zhanghao.557 <zhanghao.557@bytedance.com>
2024-06-24 17:57:58 +0800

    up

--- hello.cpp
+++ hello.cpp
@@ -1,30 +1,79 @@
-#include <atomic>
+#include <cassert>
+#include <cstddef>
+#include <initializer_list>
 #include <iostream>
-#include <thread>
+using namespace std;
 
-class SpinLock
-{
-public:
-    SpinLock() : m_flag(ATOMIC_FLAG_INIT) {}
-
-    void lock()
+template <typename T>
+class Vector {
+ public:
+    Vector(initializer_list<T> list) : m_capacity(list.size()), m_size(list.size())
     {
-        while(m_flag.test_and_set(std::memory_order_acquire)) 
-            ;
+        m_data = new T[list.size()];
+        copy(list.begin(), list.end(), m_data);
+    }
+    explicit Vector(size_t n = 0) : m_capacity(n), m_size(0), m_data(new T[m_capacity]) {}
+    ~Vector() { delete[] m_data; }
+
+    Vector(const Vector& other) : m_capacity(other.m_capacity), m_size(other.m_size) {
+        cout << "copy ctor" << endl;
+        m_data = new T[m_capacity];
+        for (int i = 0; i < other.m_size; ++i) {
+            m_data[i] = other.m_data[i];
+        }
+    }
+    Vector& operator=(const Vector& other) {
+        cout << "copy operator" << endl;
+        if (this != &other) {
+            m_capacity = other.m_capacity;
+            m_size = other.m_size;
+            delete[] m_data;
+            m_data = new T[m_capacity];
+            for (int i = 0; i < other.m_size; ++i) {
+                m_data[i] = other.m_data[i];
+            }
+        }
+        return *this;
     }
 
-    void unlock()
-    {
-        m_flag.clear(std::memory_order_release);
+    Vector(Vector&& other) : m_capacity(other.m_capacity), m_size(other.m_size), m_data(other.m_data) {
+        cout << "move ctor" << endl;
+        other.m_data = nullptr;
     }
-private:
-    std::atomic_flag m_flag;
+
+    T& operator[](size_t index) {
+        assert(index >= 0 && index < m_size);
+        return m_data[index];
+    }
+
+    size_t size() { return m_size; }
+
+    void push_back(const T& data) {
+        extend();
+        m_data[m_size++] = data;
+    }
+
+ private:
+    void extend() {
+        if (m_size + 1 <= m_capacity) return;
+        m_capacity = m_capacity == 0 ? 1 : m_capacity <<= 1;
+        auto old_data = m_data;
+        m_data = new T[m_capacity];
+        for (int i = 0; i < m_size; ++i) {
+            m_data[i] = old_data[i];
+        }
+    }
+
+ private:
+    size_t m_capacity;
+    size_t m_size;
+    T* m_data;
 };
 
-int main()
-{
-    SpinLock locker;
-    locker.lock();
-    std::cout << 1;
-    locker.unlock();
+int main() {
+    Vector<int> vec = {1, 2, 3};
+    Vector<int> vec2(std::move(vec));
+    for (int i = 0; i < vec2.size(); ++i) {
+        cout << vec2[i] << " ";
+    }
 }
