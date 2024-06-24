9c0ef1b020edf13c5ae363f8f6c2d5b935a007ef
zhanghao.557 <zhanghao.557@bytedance.com>
2024-06-24 14:54:56 +0800

    up

--- spinlock.cpp
+++ spinlock.cpp
@@ -5,11 +5,19 @@ class SpinLock
 {
 public:
     SpinLock() : m_flag(ATOMIC_FLAG_INIT) {}
+    SpinLock(const SpinLock&) = delete;
+    SpinLock& operator=(const SpinLock&) = delete;
 
     void lock()
     {
         while(m_flag.test_and_set(std::memory_order_acquire)) 
             ;
+    }
+
+    // 尝试获取锁，若没被上锁返回true
+    bool try_lock()
+    {
+        return !m_flag.test_and_set(std::memory_order_acquire);
     }
 
     void unlock()
