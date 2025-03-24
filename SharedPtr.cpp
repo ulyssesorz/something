3529655d0e6e6cb136411abfa0c4e50220b1db7a
ulyssesorz <2284903150@qq.com>
2025-03-24 16:22:09 +0800

    up

--- SharedPtr.cpp
+++ SharedPtr.cpp
@@ -1,3 +1,4 @@
+#include <atomic>
 #include <iostream>
 using namespace std;
 
@@ -5,55 +6,70 @@ template<typename T>
 class SharedPtr
 {
 public:
-    SharedPtr(T* ptr) : m_ptr(ptr), m_count(new size_t(1)) {}
-
+    explicit SharedPtr(T* ptr) : m_ptr(ptr), m_count(new atomic<size_t>(1)) {cout << "ctor" << endl;}
+    SharedPtr() : m_ptr(nullptr), m_count(nullptr) {}
     ~SharedPtr()
     {
-        cout << "dtor" << " count = " << *(m_count) << endl;
-        if(--(*m_count) == 0)
+        if(m_count && --(*m_count) == 0)
         {
             delete m_ptr;
             delete m_count;
         }
     }
-
-    SharedPtr(const SharedPtr& other) : m_ptr(other.m_ptr), m_count(other.m_count)
+    SharedPtr(const SharedPtr& rhs) : m_ptr(rhs.m_ptr), m_count(rhs.m_count)
     {
         cout << "copy ctor" << endl;
-        ++(*m_count);
-    }
-
-    SharedPtr& operator=(const SharedPtr& other)
-    {
-        cout << "copy operator" << endl;
-        if(this != &other)
+        if(m_count)
         {
-            if(--(*m_count) == 0)
+            ++(*m_count);
+        }
+    }
+    SharedPtr& operator=(const SharedPtr& rhs)
+    {
+        cout << "copy op" << endl;
+        if(this != &rhs)
+        {
+            if(m_count && --(*m_count) == 0)
             {
                 delete m_ptr;
                 delete m_count;
             }
-            m_ptr = other.m_ptr;
-            m_count = other.m_count;
+            m_ptr = rhs.m_ptr;
+            m_count = rhs.m_count;
             ++(*m_count);
         }
         return *this;
     }
 
-    T& operator*()
+    SharedPtr(SharedPtr&& rhs) : m_ptr(rhs.m_ptr), m_count(rhs.m_count)
     {
-        return *m_ptr;
+        cout << "move ctor" << endl;
+        rhs.m_ptr = nullptr;
+        rhs.m_count = nullptr;
     }
-
+    SharedPtr& operator=(SharedPtr&& rhs)
+    {
+        cout << "move op" << endl;
+        if(this != &rhs)
+        {
+            if(m_count && --(*m_count) == 0)
+            {
+                delete m_ptr;
+                delete m_count;
+            }
+            m_ptr = rhs.m_ptr;
+            m_count = rhs.m_count;
+            rhs.m_ptr = nullptr;
+            rhs.m_count = nullptr;
+        }
+        return *this;  
+    }
 private:
     T* m_ptr;
-    size_t* m_count;
+    atomic<size_t>* m_count;
 };
 
-int main()
-{
-    SharedPtr<int> sp(new int(1));
-    SharedPtr<int> sp2(nullptr);
-    sp2 = sp;
-    cout << *sp2 << endl;
+int main() {
+    SharedPtr<int> sp(nullptr);
+    SharedPtr<int> sp2(move(sp));
 }
